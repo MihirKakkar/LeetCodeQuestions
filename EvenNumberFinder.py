2b8e9f15d5892c3c463abb1dfe483eb6f99f025d
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-01-04 01:42:15 -0500

    EvenNumberFinder
    Finds number of values in an array with even number of digits

---
+++ EvenNumberFinder.py
@@ -0,0 +1,8 @@
+class Solution(object)
+    def EvenNumberFinder(self, nums):
+        for i in nums:
+            if nums % 2 == 0:
+                print(i)
+            return
+
+    self.nums = [1,2,3,4,5,65]
