f5afde4c3e9e40d1c635552c832d9df7f803433e
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-01-17 00:59:38 -0500

    The finished String Reverser
    try catch not needed

--- StringReverser.py
+++ StringReverser.py
@@ -1,3 +1,10 @@
 #String Reverser
 
 def StringReverser(stringinput):
+    try:
+        stringinput = str(stringinput)
+        return stringinput[::-1]
+        pass
+    except Exception as e:
+        print("Invalid input!")
+        raise
