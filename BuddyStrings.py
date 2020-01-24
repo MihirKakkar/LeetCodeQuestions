94f2a3021f427ba3415087f849ce25b8cc362ad9
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-01-24 00:11:35 -0500

    hashtable

--- BuddyStrings.py
+++ BuddyStrings.py
@@ -1,26 +1,22 @@
-# Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
+# # Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
 
 def BuddyStrings(A, B):
-    #making it lower
-    A = str(A.lower())
-    B = str(B.lower())
-    ArrayA = list(A)
-    ArrayB = list(B)
-
-    Alength = len(A)
-
     if len(A) != len(B):
         return False
 
-    for i in range(0, Alength):
-        i += 1
-        #initial checker
-        if (ArrayA[i] == ArrayB[i]):
-            return True
-        else:
-            #one swap
-            ArrayA[i], ArrayA[i+1] == ArrayA[i+1], ArrayA[i]
-            if (ArrayA[i] == ArrayB[i]):
+        table = {}
+
+        for z in range(len(A)):
+            if A[i] != B[i]:
+                table[i] = A[i]
+
+            if len(table) == 2:
+                i,j = table.keys()
+                A = list(A)
+                A[i] , A[j] = A[j], A[i]
+                return "".join(A) == B
+            if len(table) == 0 and A == B and len(A) > len(set(A)):
                 return True
             else:
                 return False
+#Copied from someone to learn hash table use
