dcb4ca6250681cf13a806c5ce5b8259dedc902d1
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-02-12 00:13:54 -0500

    full soln

--- BalancedStringSplit.py
+++ BalancedStringSplit.py
@@ -1,2 +1,14 @@
-def balancesStringSplit(s):
- 
+def balancedStringSplit(s):
+  counter = 0
+  res = 0
+  for i in s:
+    if i == 'L':
+        counter += 1
+    else:
+        counter -= 1
+    if counter == 0:
+        res += 1
+  return res
+
+print(balancedStringSplit("LRLRLRL"))
+        
