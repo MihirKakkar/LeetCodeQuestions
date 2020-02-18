153388e81fb5b36441c263e71bc5d512d672cb91
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-02-17 22:21:18 -0500

    updated replace elements

--- replaceElements.py
+++ replaceElements.py
@@ -1,5 +1,9 @@
-Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
-
-After doing so, return the array.
+# Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+#
+# After doing so, return the array.
 def replaceElements(arr):
-  
+    for i in arr:
+        if arr[i+1] > arr[i]:
+            arr[i+1] = arr[i] and arr[i] = arr[i+1]
+            arr[:-1] = -1
+            
