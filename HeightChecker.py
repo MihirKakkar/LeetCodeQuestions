e8f33f9d452bb1c966b6157958e910d621827690
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-02-23 01:22:38 -0500

    complete

--- HeightChecker.py
+++ HeightChecker.py
@@ -1,5 +1,12 @@
-Students are asked to stand in non-decreasing order of heights for an annual photo.
-
-Return the minimum number of students that must move in order for all students to be standing in non-decreasing order of height.
+# Students are asked to stand in non-decreasing order of heights for an annual photo.
+#
+# Return the minimum number of students that must move in order for all students to be standing in non-decreasing order of height.
 
 def heightChecker(heights):
+    counter = 0
+    for i, z in zip(heights, sorted(heights)):
+        if i != z:
+            counter += 1
+    return counter
+
+print(heightChecker([1,1,4,2,1,3]))
