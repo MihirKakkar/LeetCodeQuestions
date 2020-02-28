74b2f107793f735fdd7bd070f76e044becbeafba
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-02-28 01:03:12 -0500

    soln

--- sortedSquares.py
+++ sortedSquares.py
@@ -1,1 +1,4 @@
-Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+# Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+def sortedSquares(A):
+    return sorted([x*x for x in A])
