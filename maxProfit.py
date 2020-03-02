6d9959e4b7c926d6225900e8b6108195dc682675
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-03-02 00:25:22 -0500

    logic

--- maxProfit.py
+++ maxProfit.py
@@ -1,5 +1,20 @@
-Say you have an array for which the ith element is the price of a given stock on day i.
+# Say you have an array for which the ith element is the price of a given stock on day i.
+#
+# If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+#
+# Note that you cannot sell a stock before you buy one.
 
-If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+def maxProfit(prices):
+    if len(prices) == 0:
+        return 0
 
-Note that you cannot sell a stock before you buy one.
+    minimumPrice = prices[0]
+    maximumProf = 0
+
+    for i in prices:
+        if i < minimumPrice:
+            minimumProf = i
+        elif i - minimumPrice > maximumProf:
+            maximumProf = i - minimumPrice
+
+    return maximumProf
