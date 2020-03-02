0e979a0492936db7ad9e78c6f9e3b0015b7473c0
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-03-01 23:31:35 -0500

    proper squared

--- sortedSquares.py
+++ sortedSquares.py
@@ -1,4 +1,4 @@
 # Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
 
 def sortedSquares(A):
-    return sorted([x*x for x in A])
+    return sorted([x**2 for x in A])
