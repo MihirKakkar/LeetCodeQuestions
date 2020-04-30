8d337c1036bcc174146e28260570035ec23fd499
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-04-30 16:16:34 -0400

    fast

--- repeatedNTimes.py
+++ repeatedNTimes.py
@@ -3,13 +3,12 @@
 # Return the element repeated N times.
 
 
-def repeatedNTimes(A):
+ def repeatedNTimes(A):
     #There is no chance of another repeated number, so the question is really to just find which number repeats
-    for i in A:
-        for x in A:
-            if A[i] == A[x]:
-                return A[i]
 
+    for i in A:
+      if A.count(i) > 1:
+        return i
 # Test below
 # print(repeatedNTimes([1,2,3,3]))
 # print(repeatedNTimes([1,1,1,2,3,4]))
