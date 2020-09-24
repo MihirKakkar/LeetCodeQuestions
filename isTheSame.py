8f1c6eb9f5e808a0af9a03ae20b503fb0ee794f9
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-09-23 22:38:57 -0400

    Revert "Revert "remove try except""
    This reverts commit 2dd9c473c5f1ee72cebfd71a70f3782055df45a7.

--- isTheSame.py
+++ isTheSame.py
@@ -19,4 +19,4 @@ class Solution(object):
         if p.val != q.val:
             return False
 
-    return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
